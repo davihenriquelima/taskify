b780943c36b53b4ffdeabff49dd51191ed53b5db
davihenriquelima <davihenrique.ads@gmail.com>
2024-06-26 16:38:11 -0300

    feat: implementar salvamento no localStorage
    foram adicionadas as funções de salvamento do item que
    está sendo editado
    
    no ramo dev-davi
    mudanças a serem submetidas:
    	new file:   src/utils/localStorage.ts

---
+++ src/utils/localStorage.ts
@@ -0,0 +1,18 @@
+export const saveEditingItemLS = (id:number, text:string) => {
+    localStorage.setItem('itemEditing', id.toString());
+    localStorage.setItem('editingText', JSON.stringify(text));
+};
+
+export const loadEditingItemLS = () => {
+    const editingItem = localStorage.getItem('itemEditing');
+    const editingText = localStorage.getItem('editingText');
+    return {
+        savedEditingItem: editingItem ? Number(editingItem) : null,
+        savedEditingText: editingText ? JSON.parse(editingText) : ''
+    }
+}
+
+export const removeEditingItemLS = () => {
+    localStorage.removeItem('itemEditing');
+    localStorage.removeItem('editingText');
+};
